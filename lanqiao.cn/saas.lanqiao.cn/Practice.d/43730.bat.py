5196d63421bc787c1982f6a45bfc49dde3413a81
What410802 <what410802@126.com>
2026-04-10 21:43:06 +0800

    20260410

--- lanqiao.cn/saas.lanqiao.cn/Practice.d/43730.bat.py
+++ lanqiao.cn/saas.lanqiao.cn/Practice.d/43730.bat.py
@@ -7,7 +7,8 @@ def I(T: typing.Type):
 	return T(next(_stdin_it))
 _stdout: typing.List = [] # [Any]
 def O(*oo: typing.Any):
-	_stdout.extend(map(str, oo))
+	for o in oo:
+		_stdout.append(str(o))
 
 # import math
 # import random
@@ -17,6 +18,14 @@ def O(*oo: typing.Any):
 # import copy, functools
 
 n=I(int)
-
+ans=[]
+for i in range(n):
+	inn=I(int)
+	while(inn in ans):
+		inn+=1
+	ans.append(inn)
+O('\n')
+for i in range(n):
+	O(ans[i], ' ')
 
 sys.stdout.write("".join(_stdout))
